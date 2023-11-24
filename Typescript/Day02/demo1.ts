460be8f3f11651af444c23af278c28da2b76ad08
Raokhande sumit Ashok <raokhande.sumit@gmail.com>
2023-11-24 22:08:42 +0530

    Day02

---
+++ Typescript/Day02/demo1.ts
@@ -0,0 +1,38 @@
+
+// console.log("Hello World");
+// console.log('Welcome to Angular 12 Module');
+// console.log(`
+//         U R entering Front End World
+// `);
+
+var a1;
+var a2,a_1,a$1;
+
+// data type
+// number(int,float,double,short,long),string(''/""),boolean(true/false)
+// any(number,string,boolean)
+// void => it is neutral data type => it does not point anything 
+// misec. null & undefined=> it works as data type as well as value 
+
+var a:number=10;  // Forward declaration & defination 
+
+// console.log("value of a is "+a);
+// console.log('value of a is '+a);
+// console.log(`
+//     ----- Value of a is ${a}
+// `);
+
+var str!:string;
+// console.log("Value of str is "+str);
+
+str='Sumit';
+// console.log("Value of str is "+str);
+
+var myany:any;
+myany=2.5;
+
+console.log("Value of myany "+myany);
+myany='Sumit';
+console.log("Value of myany "+myany);
+myany=true;
+console.log("Value of myany "+myany);
