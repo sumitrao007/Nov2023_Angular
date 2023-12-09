c62f6000499490e472a10487fddf1b09c308c9c4
Raokhande sumit Ashok <raokhande.sumit@gmail.com>
2023-12-09 09:41:26 +0530

    Day01

---
+++ Angular/Day01/employee/empolyee.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector:'app-employee',
+    templateUrl:'./empolyee.component.html',
+    styleUrls:['./empolyee.component.css']
+})
+export class EmployeeComponent{
+    
+}
