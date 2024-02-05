6b9b5b528b554f76dbb4fa01c814a3aee14b6027
Raokhande sumit Ashok <raokhande.sumit@gmail.com>
2024-02-05 22:19:32 +0530

    Day20

---
+++ Angular/Day20/demotdf/demotdf.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-demotdf',
+  templateUrl: './demotdf.component.html',
+  styleUrls: ['./demotdf.component.css']
+})
+export class DemotdfComponent implements OnInit {
+
+  EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+  + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+
+  onSumit(f:NgForm){
+      console.log("Useername is "+f.value.myusername);
+  }
+
+}
